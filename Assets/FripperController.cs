01c3c068b7c3346c5963ebaa281fdeedf2c17b83
plusK2021 <81849327+plusK2021@users.noreply.github.com>
2021-05-08 21:25:12 +0900

    Initial commit

---
+++ Assets/FripperController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+
+    //hingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //左矢印キーを押した時左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //Aキーを押した時左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.A) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印キーを押した時右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //Dキーを押した時右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.D) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //Sキーあるいは下矢印を押した時左右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) )
+        {
+            SetAngle(this.flickAngle);
+        }
+
+
+        //矢印キー離された時フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow) )
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+    }
+
+
+    //フリッパーの傾きを設定
+    public void SetAngle (float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+
+}
---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    
+    //獲得した得点
+    private int score = 0;
+
+    //合計獲得得点
+    private int scoresum = 0;
+
+    //得点を表示するテキスト
+    private GameObject scorecounterText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーン中のScoreTextオブジェクトを取得
+        this.scorecounterText = GameObject.Find("ScoreText");
+
+        AddScore();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //ターゲット衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision collision)
+    {
+
+        //ターゲット毎に得点を変える
+        string tag = collision.gameObject.tag;
+
+        //SmallSterに衝突時に獲得する得点
+        if (tag == "SmallStarTag")
+        {
+            this.score =+ 5;
+        }
+
+        //LargeSterに衝突時に獲得する得点
+        else if (tag == "LargeStarTag")
+        {
+            this.score =+ 15;
+        }
+
+        //SmallCloudに衝突時に獲得する得点
+        else if (tag == "SmallCloudTag")
+        {
+            this.score =+ 20;
+        }
+
+        //LargeCloudに衝突時に獲得する得点
+        else if (tag == "LargeCloudTag")
+        {
+            this.score =+ 50;
+        }
+
+        //ターゲット以外衝突時
+        else
+        {
+            this.score = 0;
+        }
+
+        //獲得した得点をscoresumに加算する
+        this.scoresum += this.score;
+
+        AddScore();
+
+    }
+
+    //ScoreTextに得点を表示
+    void AddScore()
+    {
+        this.scorecounterText.GetComponent<Text>().text = "Score:" + (this.scoresum);
+    }
+}
